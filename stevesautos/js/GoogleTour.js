ce8ddc4a3635ea00298771c95e50110272cb36c6
Adam Powers <adam@powe.rs>
2014-05-13 15:41:48 -0700

    add 2nd site bones and move files

---
+++ stevesautos/js/GoogleTour.js
@@ -0,0 +1,20 @@
+﻿var tourLocation = "googleTour";
+
+function initialize() {
+    var bryantPark = new google.maps.LatLng(32.808826, -117.119466);
+    var panoramaOptions = {
+        position: bryantPark,
+        pov: {
+            heading: 20,
+            pitch: 0
+        },
+        zoom: 1
+    };
+    var myPano = new google.maps.StreetViewPanorama(
+        document.getElementById(tourLocation),
+        panoramaOptions);
+    myPano.setVisible(true);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
